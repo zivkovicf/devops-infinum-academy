396bf37b3959eabd5e428c7989aa838de0f05809
Filip Zivkovic <zivkovicf96@gmail.com>
2022-10-14 18:09:58 +0200

    Renamed container

--- 01-introduction/01-homework.tf
+++ 01-introduction/01-homework.tf
@@ -10,15 +10,15 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "docker" {
-  name           = "docker/getting-started:latest"
+  name         = "docker/getting-started:latest"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.docker.image_id
-  name  = "docker_tutorial"
+  name  = "cool_container"
   ports {
-    internal   = 80
+    internal = 80
     external = 80
   }
 }
