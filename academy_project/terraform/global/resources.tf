bc49e71b3ba89ee7cf036b891ca14ab5e7bf990a
Filip Zivkovic <zivkovicf96@gmail.com>
2022-11-09 17:57:44 +0100

    Added GHA user to global resources

--- academy_project/terraform/global/resources.tf
+++ academy_project/terraform/global/resources.tf
@@ -1,3 +1,27 @@
+/////////////////
+//  BEGIN DATA
+////////////////
+
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+/////////////////
+//  END DATA
+////////////////
+
+
+////////////////////
+//  BEGIN RESOURCES
+////////////////////
+
 resource "aws_ecr_repository" "academy_ecr" {
   name                 = "academy"
   image_tag_mutability = "MUTABLE"
@@ -27,15 +51,55 @@ resource "aws_cloudwatch_metric_alarm" "billing_alarm" {
     Currency = "USD"
   }
 }
-data "aws_iam_policy_document" "instance-assume-role-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+resource "aws_iam_user" "ecr" {
+  name = "ecr"
 }
 
+resource "aws_iam_policy" "ecr_rw" {
+  name        = "allowEcrPushPull"
+  description = "Allows push and pulling inmages from academy repo"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "AllowPushPull",
+          "Effect": "Allow",
+          "Action": [
+              "ecr:BatchGetImage",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:CompleteLayerUpload",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:InitiateLayerUpload",
+              "ecr:PutImage",
+              "ecr:UploadLayerPart"
+          ],
+          "Resource": "${aws_ecr_repository.academy_ecr.arn}"
+      },
+      {
+          "Sid": "AllowAuthorizationToken",
+          "Effect": "Allow",
+          "Action": "ecr:GetAuthorizationToken",
+          "Resource": "*"
+      }
+  ]
+}
+  EOF
+}
 
+resource "aws_iam_user_policy_attachment" "ecr_rw_attachment" {
+  user       = aws_iam_user.ecr.name
+  policy_arn = aws_iam_policy.ecr_rw.arn
+}
+
+resource "aws_iam_access_key" "ecr_acces_key" {
+  user = aws_iam_user.ecr.name
+}
+
+resource "aws_ssm_parameter" "ecr_secret_key" {
+  name        = "/terraform/iam/ecr/secret_key"
+  description = "Secret key for acces key ID"
+  type        = "SecureString"
+  value       = aws_iam_access_key.ecr_acces_key.secret
+}
